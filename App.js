990440e93a42361f1c7a78b6d8ea28496df7a070
Efrain A. Davila <105945226+EfrainAD@users.noreply.github.com>
2023-03-30 13:56:24 -0400

    Adjusted the display to work on both android and ios

--- App.js
+++ App.js
@@ -1,19 +1,22 @@
-import { StatusBar } from 'expo-status-bar'
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar'
+import { SafeAreaView, StyleSheet, Text, View, StatusBar } from 'react-native'
 
 export default function App() {
    return (
-      <SafeAreaView style={{flex: 1}}>
-         <View style={{backgroundColor: 'blue', padding: 16, alignItems: 'center'}}>
-            <Text>MeatsToGo Canvas</Text>
-         </View>
-         <View style={{backgroundColor: 'red', flex: 1,  padding: 10, alignItems: 'center'}}>
-            <Text style={{color: 'white'}}>Hello World!</Text>
-            <Text style={{color: 'white'}}>Hello World!</Text>
-            <Text style={{color: 'white'}}>Hello World!</Text>
-            <Text style={{color: 'white'}}>Hello World!</Text>
-         </View>
-      </SafeAreaView>
+      <>
+         <SafeAreaView style={{flex: 1}}>
+            <View style={{backgroundColor: 'blue', padding: 16, marginTop: StatusBar.currentHeight, alignItems: 'center'}}>
+               <Text>MeatsToGo Canvas</Text>
+            </View>
+            <View style={{backgroundColor: 'red', flex: 1,  padding: 10, alignItems: 'center'}}>
+               <Text style={{color: 'white'}}>Hello World!</Text>
+               <Text style={{color: 'white'}}>Hello World!</Text>
+               <Text style={{color: 'white'}}>Hello World!</Text>
+               <Text style={{color: 'white'}}>Hello World!</Text>
+            </View>
+         </SafeAreaView> 
+         <ExpoStatusBar style='auto' />
+      </>
    )
 }
 
