478f76cd27f70398b877fe2836a687334db2e1d4
Efrain A. Davila <105945226+EfrainAD@users.noreply.github.com>
2023-03-30 20:36:48 -0400

    Moved the styles into the react native StyleSheet

--- App.js
+++ App.js
@@ -1,24 +1,34 @@
+import React from 'react'
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar'
 import { SafeAreaView, StyleSheet, Text, View, StatusBar } from 'react-native'
 
 export default function App() {
    return (
       <>
-         <SafeAreaView style={{flex: 1}}>
-            <View style={{backgroundColor: 'blue', padding: 16, marginTop: StatusBar.currentHeight, alignItems: 'center'}}>
+         <SafeAreaView style={styles.container}>
+            <View style={styles.search}>
                <Text>MeatsToGo Canvas</Text>
             </View>
-            <View style={{backgroundColor: 'red', flex: 1,  padding: 10, alignItems: 'center'}}>
-               <Text style={{color: 'white'}}>Hello World!</Text>
-               <Text style={{color: 'white'}}>Hello World!</Text>
-               <Text style={{color: 'white'}}>Hello World!</Text>
-               <Text style={{color: 'white'}}>Hello World!</Text>
+            <View style={styles.list}>
+               <Text style={styles.listItem}>Hello World!</Text>
+               <Text style={styles.listItem}>Hello World!</Text>
+               <Text style={styles.listItem}>Hello World!</Text>
+               <Text style={styles.listItem}>Hello World!</Text>
             </View>
-         </SafeAreaView> 
-         <ExpoStatusBar style='auto' />
+         </SafeAreaView>
+         <ExpoStatusBar style="auto" />
       </>
    )
 }
 
 const styles = StyleSheet.create({
+   container: { flex: 1 },
+   search: {
+      backgroundColor: 'blue',
+      padding: 16,
+      marginTop: StatusBar.currentHeight,
+      alignItems: 'center',
+   },
+   list: { backgroundColor: 'red', flex: 1, padding: 10, alignItems: 'center' },
+   listItem: { color: 'white' },
 })
