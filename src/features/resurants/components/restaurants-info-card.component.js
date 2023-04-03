aac19ff8e4d25aba182dfaffa5525d8b9c3092df
Efrain A. Davila <105945226+EfrainAD@users.noreply.github.com>
2023-04-03 14:32:38 -0400

    made a Address component, an moved title under the emg

--- src/features/resurants/components/restaurants-info-card.component.js
+++ src/features/resurants/components/restaurants-info-card.component.js
@@ -1,28 +1,31 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Text } from 'react-native'
+import { Text, View } from 'react-native'
 import { Card } from 'react-native-paper'
 
 /* padding: 10px; */
 const RestaurantCard = styled(Card)`
    background: ${(props) => props.theme.colors.bg.primary};
    margin: ${(props) => props.theme.space[2]};
-   padding: 0 ${(props) => props.theme.space[3]};
+   padding: ${(props) => props.theme.space[3]};
 `
 const Title = styled(Text)`
    font-family: ${(props) => props.theme.fonts.body};
    color: ${(props) => props.theme.colors.text.primary};
-   font-weight: bold;
+   font-weight: ${(props) => props.theme.fontWeights.bold};
    text-align: center;
    font-size: ${(props) => props.theme.fontSizes.title};
-   padding: ${(props) => props.theme.space[3]};
+   padding-bottom: ${(props) => props.theme.space[1]};
 `
-const Content = styled(Text)`
+const Address = styled(Text)`
+   font-family: ${(props) => props.theme.fonts.body};
+   font-size: ${(props) => props.theme.fontSizes.caption};
    color: ${(props) => props.theme.colors.text.primary};
-   padding: ${(props) => props.theme.space[3]};
    text-align: center;
 `
-
+const Info = styled(View)`
+   padding: ${(props) => props.theme.space[4]};
+`
 export const RestaurantInfoCard = (restaurant = {}) => {
    const {
       name = 'Some Restaurant',
@@ -38,9 +41,11 @@ export const RestaurantInfoCard = (restaurant = {}) => {
 
    return (
       <RestaurantCard elevation={5}>
-         <Title>{name}</Title>
          <Card.Cover source={{ uri: photos[0] }} />
-         <Content>{address}</Content>
+         <Info>
+            <Title>{name}</Title>
+            <Address>{address}</Address>
+         </Info>
       </RestaurantCard>
    )
 }
