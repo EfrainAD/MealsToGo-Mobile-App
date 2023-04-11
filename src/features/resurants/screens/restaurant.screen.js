336f9509d84b5ea447d738a3819738dd1192a4a3
Efrain A. Davila <105945226+EfrainAD@users.noreply.github.com>
2023-04-11 15:03:17 -0400

    Added FlatList to the restaurant screen

--- src/features/resurants/screens/restaurant.screen.js
+++ src/features/resurants/screens/restaurant.screen.js
@@ -1,5 +1,11 @@
 import React, { useState } from 'react'
-import { SafeAreaView, View, StatusBar } from 'react-native'
+import {
+   SafeAreaView,
+   FlatList,
+   View,
+   StatusBar,
+   StyleSheet,
+} from 'react-native'
 import { Searchbar } from 'react-native-paper'
 import styled from 'styled-components'
 import { RestaurantInfoCard } from '../components/restaurants-info-card.component'
@@ -11,12 +17,6 @@ const SearchbarContainer = styled(View)`
    background-color: ${(props) => props.theme.colors.brand.primary};
    padding: ${(props) => props.theme.space[3]};
    ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px;`}
-`
-const ListContainer = styled(View)`
-   background-color: ${(props) => props.theme.colors.ui.secondary};
-   flex: 1;
-   padding: ${(props) => props.theme.space[2]};
-   align-items: center;
 `
 
 export const RestaurantsScreen = () => {
@@ -31,12 +31,37 @@ export const RestaurantsScreen = () => {
                value={searchQuery}
             />
          </SearchbarContainer>
-         <ListContainer>
-            <RestaurantInfoCard />
-            <RestaurantInfoCard />
-            <RestaurantInfoCard />
-            <RestaurantInfoCard />
-         </ListContainer>
+         <FlatList
+            data={[
+               { name: 0 },
+               { name: 1 },
+               { name: 2 },
+               { name: 3 },
+               { name: 4 },
+               { name: 5 },
+               { name: 6 },
+               { name: 7 },
+               { name: 8 },
+               { name: 9 },
+               { name: 10 },
+               { name: 11 },
+               { name: 12 },
+               { name: 13 },
+               { name: 14 },
+               { name: 15 },
+               { name: 16 },
+               { name: 17 },
+               { name: 18 },
+               { name: 19 },
+            ]}
+            renderItem={() => <RestaurantInfoCard />}
+            keyExtractor={(item) => item.name}
+            contentContainerStyle={styles.flatList}
+         />
       </Container>
    )
 }
+
+const styles = StyleSheet.create({
+   flatList: { padding: 16 },
+})
