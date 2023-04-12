df2a33e4d0d2a2215f3c5745779845d536e48738
Efrain A. Davila <105945226+EfrainAD@users.noreply.github.com>
2023-04-11 20:21:18 -0400

    Complete navbar challenge

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import React from 'react'
+import { Text, View } from 'react-native'
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar'
 import { RestaurantsScreen } from './src/features/resurants/screens/restaurant.screen'
+import { NavigationContainer } from '@react-navigation/native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { ThemeProvider } from 'styled-components/native'
 import { theme } from './src/infrastructure/theme'
 import {
@@ -8,6 +11,23 @@ import {
    Oswald_400Regular,
 } from '@expo-google-fonts/oswald'
 import { useFonts as useLato, Lato_400Regular } from '@expo-google-fonts/lato'
+
+function MapsScreen() {
+   return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+         <Text>Maps!</Text>
+      </View>
+   )
+}
+
+function SettingsScreen() {
+   return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+         <Text>Settings!</Text>
+      </View>
+   )
+}
+const Tab = createBottomTabNavigator()
 
 export default function App() {
    const [oswaldLoaded] = useOswald({ Oswald_400Regular })
@@ -17,7 +37,13 @@ export default function App() {
    return (
       <>
          <ThemeProvider theme={theme}>
-            <RestaurantsScreen />
+            <NavigationContainer>
+               <Tab.Navigator>
+                  <Tab.Screen name="Restaurant" component={RestaurantsScreen} />
+                  <Tab.Screen name="Maps" component={MapsScreen} />
+                  <Tab.Screen name="Settings" component={SettingsScreen} />
+               </Tab.Navigator>
+            </NavigationContainer>
          </ThemeProvider>
          <ExpoStatusBar style="auto" />
       </>
