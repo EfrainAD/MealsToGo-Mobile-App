f1dec79e4c71be7d2a03899a44b8386babbd576f
Efrain A. Davila <105945226+EfrainAD@users.noreply.github.com>
2023-04-15 14:43:44 -0400

    Now get data and passed into the restaurant context what was set up

--- src/services/restaurants/restaurants.context.js
+++ src/services/restaurants/restaurants.context.js
@@ -1,10 +1,35 @@
-import React, { createContext } from 'react'
+import React, { createContext, useState, useEffect } from 'react'
+import { restaurantsRequest, restaurantsTransform } from './restaurants.service'
 
 export const RestaurantsContext = createContext()
 
 export const RestaurantsContextProvider = ({ children }) => {
+   const [restaurants, setRestaurants] = useState([])
+   const [isLoading, setIsLoading] = useState(false)
+   const [error, setError] = useState(null)
+
+   const retrieveRestaunts = () => {
+      setIsLoading(true)
+      setTimeout(() => {
+         restaurantsRequest()
+            .then(restaurantsTransform)
+            .then((results) => {
+               setIsLoading(false)
+               setRestaurants(results)
+            })
+            .catch((err) => {
+               setIsLoading(false)
+               setError(err)
+            })
+      }, 2000)
+   }
+
+   useEffect(() => {
+      retrieveRestaunts()
+   }, [])
+
    return (
-      <RestaurantsContext.Provider value={{ restaurants: [1, 2, 3] }}>
+      <RestaurantsContext.Provider value={{ restaurants, isLoading, error }}>
          {children}
       </RestaurantsContext.Provider>
    )
